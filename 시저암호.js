acbd99074837fa335bf4811d2b9a54699baa40e4
leeejin <asd72621@naver.com>
2024-05-23 21:12:57 +0900

    2024-05-23

--- 시저암호.js
+++ 시저암호.js
@@ -1,20 +1,16 @@
 function solution(s, n) {
-    var answer = '';
-    var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var lower = "abcdefghijklmnopqrstuvwxyz";
-    
-    for(let i =0; i<s.length; i++){
-        if(s[i] == " ") {
-            answer += " ";
-            continue;
-        }
-        
-        let position = upper.includes(s[i]) ? upper : lower;
-        let index = position.indexOf(s[i])+n;
-        if(index>= position.length){
-            index -= position.length;
-        }
-        answer += position[index];
+  var answer = "";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === " ") {
+      answer += " ";
+      continue;
     }
-    return answer;
+    const alphabet = upper.includes(s[i]) ? upper : lower;
+    const idx = alphabet.indexOf(s[i]) + n;
+    const newIdx = idx > alphabet.length - 1 ? idx - alphabet.length : idx;
+    answer += alphabet[newIdx];
+  }
+  return answer;
 }
